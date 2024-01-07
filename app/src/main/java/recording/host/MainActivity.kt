9576c6590faceea97a91eac2248477e11329cfb8
asywalulfikri <asywalul.fikri@ottodigital.id>
2024-01-07 20:49:28 +0700

    Initial commit

---
+++ app/src/main/java/recording/host/MainActivity.kt
@@ -0,0 +1,12 @@
+package recording.host
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
